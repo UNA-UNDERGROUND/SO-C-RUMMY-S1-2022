45ed9381ba3fc718bcf049432f9692c658fa76c1
Jonathan Guzmán <jonguz5@hotmail.com>
2022-03-30 13:00:58 -0600

    add structs

---
+++ source/include/rummy/game/hand.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Hand C struct
+typedef struct {
+	int id;
+	int *cards;
+	int size;
+} Hand;
---
+++ source/include/rummy/game/player.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <rummy/game/hand.h>
+
+typedef struct {
+	int id;
+	Hand *hand;
+} Player;
