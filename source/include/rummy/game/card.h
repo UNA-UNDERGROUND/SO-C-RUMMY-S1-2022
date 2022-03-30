a11cc4d69b9fd892d3e7337bc6172abbe79238b2
Jonathan Guzmán <jonguz5@hotmail.com>
2022-03-30 12:26:49 -0600

    add card

---
+++ source/include/rummy/game/card.h
@@ -0,0 +1,19 @@
+#pragma once
+
+// -1 is the joker or wildcard
+
+typedef struct {
+	int color;
+	int value;
+} Card;
+
+/**
+ * @brief Create a Card object
+ *
+ * @note if the color or value are invalid, the card will be a joker
+ *
+ * @param color the color of the card
+ * @param value the value of the card
+ * @return Card* the card created
+ */
+Card *createCard(int color, int value);
---
+++ source/src/game/card.c
@@ -0,0 +1,13 @@
+#include <rummy/game/card.h>
+#include <stdlib.h>
+
+Card *createCard(int color, int value) {
+	Card *card = (Card *)malloc(sizeof(Card));
+	card->color = color;
+	card->value = value;
+	if (color < 0 || color > 3 || value < 0 || value > 13) {
+		card->color = -1;
+		card->value = -1;
+	}
+	return card;
+}
