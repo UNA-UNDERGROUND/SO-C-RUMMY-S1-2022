6d5ad2c677aac41f9096a02d6b9e4a8bf48ef71c
Jonathan Guzmán <jonguz5@hotmail.com>
2022-03-31 10:43:59 -0600

    moved common definitions

--- source/include/rummy/util/memory/allocator.h
+++ source/include/rummy/util/memory/allocator.h
@@ -1,14 +1,7 @@
 #pragma once
-#include <stddef.h>
-#include <stdlib.h>
-// contains code used for memory allocation/deallocation and management
+#include <rummy/util/memory/common.h>
 
-/**
- * @brief Function deleter for a list of pointers in a array, useful for objects
- * that need custom deallocation
- *
- */
-typedef void (*Deleter)(void *);
+// contains code used for memory allocation/deallocation and management
 
 /**
  * @brief tries to allocate memory for an array of size elements
@@ -31,3 +24,38 @@ void **alloc_array(size_t size);
  * null just free the memory
  */
 void free_array(void **ptr, size_t size, Deleter deleter);
+
+/**
+ * @brief copies the content of an array of pointers to another array of
+ * pointers
+ *
+ * @note an assertion is raised if the size of the source array is greater than
+ * the size of the destination array
+ *
+ * @param src the source array if null, nothing is done
+ * @param dst the destination array if null, nothing is done
+ * @param size_src the size of the source array
+ * @param size_dst the size of the destination array
+ * @param copier the function to call to copy the content of each element, if
+ * NULL it will copy the memory
+ *
+ */
+void copy_array(void **src, void **dst, size_t size_src, size_t size_dst,
+                Copier copier);
+
+/**
+ * @brief moves the content of an array of pointers to another array of pointers
+ *
+ * @note an assertion is raised if the size of the source array is greater than
+ * the size of the destination array
+ *
+ * @param src the source array if null, nothing is done
+ * @param dst the destination array if null, nothing is done
+ * @param size_src the size of the source array
+ * @param size_dst the size of the destination array
+ * @param mover the function to call to move the content of each element, if
+ * NULL it will move the memory
+ *
+ */
+void move_array(void **src, void **dst, size_t size_src, size_t size_dst,
+                Mover mover);
