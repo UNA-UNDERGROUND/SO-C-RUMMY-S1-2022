760db2ad5b4ffebf1bf69d646202bfce63c7a292
Jonathan Guzmán <jonguz5@hotmail.com>
2022-04-02 21:25:53 -0600

    update vector

--- source/include/rummy/util/memory/vector.h
+++ source/include/rummy/util/memory/vector.h
@@ -18,6 +18,38 @@ typedef struct {
 Vector *new_vector();
 
 /**
+ * @brief push a new element to the vector
+ *
+ * @param vector the vector to push
+ * @param element the element to push
+ */
+void vector_push_back(Vector *vector, void *element);
+
+/**
+ * @brief pop the last element from the vector
+ *
+ * @param vector the vector to pop
+ * @return void* the element popped
+ */
+void vector_remove(Vector *vector, size_t index);
+
+/**
+ * @brief resize the vector using malloc
+ *
+ * @note if the new capacity is smaller the operation will assert
+ *
+ * @param vector the vector to resize
+ */
+void vector_resize(Vector *vector, size_t new_capacity);
+
+/**
+ * @brief get the element at the top of the vector
+ *
+ * @param vector the vector to get the element from
+ */
+void *vector_top(Vector *vector);
+
+/**
  * @brief Delete a Vector object
  *
  * @param vector the vector to delete, if NULL nothing is done
--- source/src/util/memory/vector.c
+++ source/src/util/memory/vector.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <rummy/util/memory/vector.h>
 #include <stdlib.h>
 
@@ -7,6 +8,45 @@ Vector *new_vector() {
 	vector->capacity = 0;
 	vector->data = NULL;
 	return vector;
+}
+
+void vector_push_back(Vector *vector, void *element) {
+	if (vector->size == vector->capacity) {
+		vector_resize(vector, vector->capacity * 2);
+	}
+	vector->data[vector->size] = element;
+	vector->size++;
+}
+
+void vector_remove(Vector *vector, size_t index) {
+	if (index >= vector->size) {
+		return;
+	}
+	for (size_t i = index; i < vector->size - 1; i++) {
+		vector->data[i] = vector->data[i + 1];
+	}
+	vector->size--;
+}
+
+void vector_resize(Vector *vector, size_t new_capacity) {
+	assert(new_capacity > 0);
+	assert(vector->size <= new_capacity);
+	// alloc a new array
+	void **new_data = malloc(new_capacity * sizeof(void *));
+	// copy old data to new array
+	for (size_t i = 0; i < vector->size; i++) {
+		new_data[i] = vector->data[i];
+	}
+	// free old array
+	free(vector->data);
+	// update vector
+	vector->data = new_data;
+	vector->capacity = new_capacity;
+}
+
+void *vector_top(Vector *vector) {
+	assert(vector->size > 0);
+	return vector->data[vector->size - 1];
 }
 
 void delete_vector(Vector *vector, Deleter deleter) {
